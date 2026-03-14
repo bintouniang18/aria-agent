5c2558419e24e57b36751647f49f9bc6a20a2a89
bintouniang18 <bintouniang18@icloud.com>
2026-03-14 01:47:50 +0100

    Update login.js

--- api/auth/login.js
+++ api/auth/login.js
@@ -1,26 +1,15 @@
 export default function handler(req, res) {
   const clientId = process.env.GOOGLE_CLIENT_ID;
-  const redirectUri = process.env.GOOGLE_REDIRECT_URI
+  const redirectUri = process.env.GOOGLE_REDIRECT_URI;
 
-  if (!clientId || !redirectUri) {
-    return res.status(500).send("OAuth config missing");
-  }
+  const authUrl = new URL("https://accounts.google.com/o/oauth2/v2/auth");
 
-  const scopes = [
-    'https://www.googleapis.com/auth/gmail.readonly',
-    'https://www.googleapis.com/auth/gmail.send',
-    'https://www.googleapis.com/auth/calendar.readonly',
-    'https://www.googleapis.com/auth/userinfo.email'
-  ].join(' ');
-
-  const authUrl = new URL('https://accounts.google.com/o/oauth2/v2/auth');
-
-  authUrl.searchParams.set('client_id', clientId);
-  authUrl.searchParams.set('redirect_uri', redirectUri);
-  authUrl.searchParams.set('response_type', 'code');
-  authUrl.searchParams.set('scope', scopes);
-  authUrl.searchParams.set('access_type', 'offline');
-  authUrl.searchParams.set('prompt', 'consent');
+  authUrl.searchParams.set("client_id", clientId);
+  authUrl.searchParams.set("redirect_uri", redirectUri);
+  authUrl.searchParams.set("response_type", "code");
+  authUrl.searchParams.set("scope", "https://www.googleapis.com/auth/gmail.readonly");
+  authUrl.searchParams.set("access_type", "offline");
+  authUrl.searchParams.set("prompt", "consent");
 
   res.redirect(authUrl.toString());
 }
